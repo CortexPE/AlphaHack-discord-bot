8393ebf0d5fbe0f095e044c8fca2a368cf3b9dc1
ArceusMatt <arceusthegod303@yahoo.com>
2016-10-19 14:22:22 -0400

    correct way

--- AlphaHack/bot.js
+++ AlphaHack/bot.js
@@ -1,7 +1,14 @@
 var Discord = require("discord.js");
-
 var alphaBot = new Discord.Client();
 
-//todo
+alphaBot.on('ready', () => {
+  console.log('Ready for usage.');
+});
 
-alphaBot.loginWithToken("Bot token");
+alphaBot.on('message', message => {
+if(message.content === '/help'){
+      message.reply('There is no help at the moment.');
+  }
+});
+
+alphaBot.login("Bot token");
