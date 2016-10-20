78b3662b2883af1654caa9b62ccb77eaeadecb31
c0rt3xg4m1ng <mcpe4life62@gmail.com>
2016-10-20 12:56:37 +0800

    Update bot.js

--- AlphaHack/bot.js
+++ AlphaHack/bot.js
@@ -7,7 +7,12 @@ try {
 	process.exit();
 }
 console.log("Starting DiscordBot\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
-
+try {
+	var msic = require("./music_plugin");
+	var msic_plugin = new msic();
+} catch(e){
+	console.log("couldn't load YT/Music plugin!\n"+e.stack);
+}
 const alphaBot = new Discord.Client();
 
 alphaBot.on('ready', () => {
